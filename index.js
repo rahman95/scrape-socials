9c8b7572b9b1f590ebc0ff8bbfdd8c21601336c3
Rahman Younus <rahman@industrious.agency>
2019-04-12 18:13:10 +0100

    added some colour - wip

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import getTwitterFollowers from './lib/twitter';
 import getInstagramFollowers from './lib/instagram';
+import chalk from 'chalk';
 
 async function run() {
-  const twFollowers = await getTwitterFollowers('rahman_younus');
-  const igFollowers = await getInstagramFollowers('rahman_younus');
+  const username = 'rahman_younus';
+  const [twFollowers, igFollowers] = await Promise.all([getTwitterFollowers(username), getInstagramFollowers(username)])
 
-  console.log(`You currently have ${twFollowers} twitter followers, and ${igFollowers} instagram followers`)
+  console.log(`You currently have ${chalk.hex('#1EA1F3').bold(twFollowers)} twitter followers, and ${chalk.hex('#C7028D').bold(igFollowers)} instagram followers`)
 }
 
 run();

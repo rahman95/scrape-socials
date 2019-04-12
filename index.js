511492132570848aaf00622cf4e9573e161ba843
Rahman Younus <rahman@industrious.agency>
2019-04-12 18:03:13 +0100

    added ig logic to get followers

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import getTwitterFollowers from './lib/twitter';
+import getInstagramFollowers from './lib/instagram';
 
 async function run() {
   const twFollowers = await getTwitterFollowers('rahman_younus');
+  const igFollowers = await getInstagramFollowers('rahman_younus');
 
-  console.log(twFollowers);
+  console.log(`You currently have ${twFollowers} twitter followers, and ${igFollowers} instagram followers`)
 }
 
 run();
